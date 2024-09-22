9802aa01c413d631e17d717b41de490820198161
Ajani Maragh <amsbstart@gmail.com>
2024-09-22 14:53:12 -0700

    reset all states to initial value and calculated coordinates

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,4 @@
-import React, {  useState} from 'react'
+import React, {  useState } from 'react'
 
 
 // Suggested initial states
@@ -18,27 +18,63 @@ export default function AppFunctional(props) {
   function getXY() {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
+    const x = index % 3 + 1
+    const y = Math.floor(index / 3) + 1
+    return [x,y]
   }
 
   function getXYMessage() {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
+    const [x, y] = getXY()
+    return `Cordinates (${x}, ${y})`
   }
 
   function reset() {
     // Use this helper to reset all states to their initial values.
+    setMessage(initialMessage)
+    setEmail(initialEmail)
+    setSteps(initialSteps)
+    setIndex(initialIndex)
+    
   }
 
   function getNextIndex(direction) {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+    const x = index % 3
+    const y = Math.floor(index / 3)
+
+  if (direction === 'left' && x > 0) {
+      return index - 1
   }
+  if (direction === 'right' && x < 2) {
+    return index + 1
+  }
+  if (direction === 'up' && y > 0) {
+    return index - 3
+  }
+  if (direction === 'down' && y < 2) {
+    return index + 3
+  }
+  return index
+}
 
   function move(evt) {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    const direction = evt.target.id
+
+    const newIndex = getNextIndex(direction)
+
+    if (newIndex !== index) {
+      setIndex(newIndex)
+      setSteps(steps + 1)
+    } else {
+      setMessage('you cant move in that direction')
+    }
   }
 
   function onChange(evt) {
@@ -68,10 +104,10 @@ export default function AppFunctional(props) {
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
+        <button id="left" onClick={move}>LEFT</button>
+        <button id="up" onClick={move}>UP</button>
+        <button id="right" onClick={move}>RIGHT</button>
+        <button id="down" onClick={move}>DOWN</button>
         <button id="reset">reset</button>
       </div>
       <form>
