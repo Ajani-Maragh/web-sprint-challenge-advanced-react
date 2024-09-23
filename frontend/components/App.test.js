d4e3d3d8c28c11d24b47eae21a224ff62a43a387
Ajani Maragh <amsbstart@gmail.com>
2024-09-22 18:01:44 -0700

    completed first test

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,21 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+import React from 'react'
+import { screen, render } from '@testing-library/react'
+import AppFunctional from './AppFunctional'
+import '@testing-library/jest-dom'
+
+
+describe('AppFunctional Component', () => {
+  beforeEach(() => {
+    render(<AppFunctional />)
+  })
+  test('renders all the text elements correctly', () => {
+    expect(screen.getByText('Coordinates (2, 2)')).toBeInTheDocument()
+    expect(screen.getByText('You moved 0 times')).toBeInTheDocument()
+    expect(screen.getByText('UP')).toBeInTheDocument()
+    expect(screen.getByText('DOWN')).toBeInTheDocument()
+    expect(screen.getByText('LEFT')).toBeInTheDocument()
+    expect(screen.getByText('RIGHT')).toBeInTheDocument()
+    expect(screen.getByText('reset')).toBeInTheDocument()
+  })
 })
