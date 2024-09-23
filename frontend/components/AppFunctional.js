dd076af70ce92bbf431b740b2e7bca3cd65527d8
Ajani Maragh <amsbstart@gmail.com>
2024-09-22 17:02:50 -0700

    fully binded everything

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,5 @@
 import React, {  useState } from 'react'
+import axios from 'axios'
 
 
 // Suggested initial states
@@ -28,7 +29,7 @@ export default function AppFunctional(props) {
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
     const [x, y] = getXY()
-    return `Cordinates (${x}, ${y})`
+    return `Coordinates (${x}, ${y})`
   }
 
   function reset() {
@@ -65,7 +66,7 @@ export default function AppFunctional(props) {
       setIndex(newIndex)
       setSteps(steps + 1)
     } else {
-      setMessage('you cant move in that direction')
+      setMessage(`You can't go ${direction}`)
     }
   }
 
@@ -78,13 +79,38 @@ export default function AppFunctional(props) {
     // Use a POST request to send a payload to the server.
     evt.preventDefault()
 
+    const [x, y] = getXY()
+    const payload = {
+      x: x,
+      y: y,
+      steps: steps,
+      email: email,
+    }
+
+    if (email ===  '') {
+      setMessage('Ouch: email is required.')
+      return
+    }
+    axios.post("http://localhost:9000/api/result", payload)
+    .then(response => {
+      setMessage(response.data.message)
+      //reset(true)
+    })
+    .catch(error => {
+      if (error.response && error.response.data.message) {
+        setMessage('An error occurred: ' + error.response.data.message)
+      } else {
+        setMessage('An error occurred: ' + error.message)
+      }
+    })
+
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">{getXYMessage()}</h3>
-        <h3 id="steps">You moved {steps} times</h3>
+        <h3 id="steps">You moved {steps} {steps === 1 ? 'time' : 'times'}</h3>
       </div>
       <div id="grid">
         {[0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
@@ -94,17 +120,17 @@ export default function AppFunctional(props) {
           ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
         <button id="left" onClick={move}>LEFT</button>
         <button id="up" onClick={move}>UP</button>
         <button id="right" onClick={move}>RIGHT</button>
         <button id="down" onClick={move}>DOWN</button>
-        <button id="reset">reset</button>
+        <button id="reset" onClick={reset}>reset</button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
+      <form onSubmit={onSubmit}>
+        <input id="email" type="email" placeholder="type email" value={email} onChange={onChange}></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
